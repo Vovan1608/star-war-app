52c53f18da03d6d8097b7e6507bc99d27b1a2873
Vladymyr Zenin <vovan1608@gmail.com>
2022-07-13 11:38:23 +0300

    refactoring WithData

--- src/cpmponents/hoc-helper/WithData.js
+++ src/cpmponents/hoc-helper/WithData.js
@@ -1,23 +1,35 @@
 import { useEffect, useState } from 'react';
 
 import Spinner from '../spinner';
+import ErrorBoundary from '../errorBoundary';
 
 const WithData = (Component) => {
     return function Wrapp(props) {
         const [data, setData] = useState([]);
+        const [isError, setIsError] = useState(false);
+        const [isLoading, setIsLoading] = useState(true);
 
         useEffect(() => {
             props.getData()
                 .then((data) => {
                     setData(data);
+                    setIsLoading(false);
+                })
+                .catch(() => {
+                    setIsError(true);
                 });
         }, []);
 
-        if (!data) {
+        if (isLoading) {
             return <Spinner />;
         }
 
-        return <Component {...props} data={data} />;
+        return (
+            <ErrorBoundary error={isError}>
+                <Component {...props} data={data} />
+            </ErrorBoundary>
+
+        );
     };
 };
 
